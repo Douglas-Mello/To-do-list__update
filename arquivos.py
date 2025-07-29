145ae1243e886409c1e9f72642e80a37bbc1e7a5
Douglas Mello <douglasaugusto.mello@gmail.com>
2025-07-28 23:16:36 -0300

    case 2 feito

--- arquivos.py
+++ arquivos.py
@@ -1,19 +1,45 @@
 pessoa = dict()
-tarefas= list()
+
 def lista(opc):
+    tarefas= list()
     match opc:
         case 1:
             with open("ListadeTarefa.txt", 'a+') as file:
                 nome=input('nome da pessoa: ')
                 c=1
+                tarefa=input(f"Digite a primeira Tarefa de {nome}: ")
+                pessoa[nome]=[tarefa]
                 for k, i in pessoa.items():
                     
-                    file.write(f"Nome {k} Tarefas: {c} --> {i} \n")
-                    file.write("\n")
+                    file.write(f"Nome {k} Tarefas: {c} --> {i}\n")
                     c+=1
         case 2:
-            with open("ListadeTarefa.txt", 'r') as file:
+            print("Quer adicionar tarefa a quem: ")
+            for k in pessoa.keys():
+                print(f"- {k}")
+            adicionar = input("Sua escolha: ")
+            if adicionar not in pessoa:
+                print("Pessoa não encontrada.")
+                return 
 
+            while True:
+                novaTarefa = input("Tarefa a ser adicionada: ")
+                tarefas.append(novaTarefa)
+                cont = input("Quer adicionar mais tarefas [s/n]: ").strip().lower()
+                
+                if cont == 'n':
+                    pessoa[adicionar] += tarefas
+                    with open("ListadeTarefa.txt", "r") as file:
+                        linhas = file.readlines()
+                    with open("ListadeTarefa.txt", "w") as file:
+                        for linha in linhas:
+                            if f"Nome {adicionar} " in linha: #Verifica se a pessoa q vou mexer está nal inha
+                                file.write(f"Nome {adicionar} Tarefas: {len(pessoa[adicionar])} --> {pessoa[adicionar]}\n")
+                            else:
+                                file.write(linha)
+                    break
+
+                
         
 
                
